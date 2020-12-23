203fdabb2d411fbc3d3294eea928bab5dc4eb7c2
juan.wang <juan.wang@itrigger.com>
2020-12-23 23:59:27 +0800

    spring-autoconfigure-metadata.properties实现条件装配3.0

--- src/main/java/com/gupaoedu/autoconfiguration/GupaoConfiguration.java
+++ src/main/java/com/gupaoedu/autoconfiguration/GupaoConfiguration.java
@@ -1,11 +1,9 @@
 package com.gupaoedu.autoconfiguration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisOperations;
 
-@ConditionalOnClass({RedisOperations.class})
+/*@ConditionalOnClass({RedisOperations.class})*/
 @Configuration
 public class GupaoConfiguration {
 
